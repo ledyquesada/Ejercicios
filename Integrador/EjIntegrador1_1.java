1f5e6ac42c75b159eefe39cd183a8534b3b1e911
ledyquesada <ledyquesada@gmail.com>
2024-01-15 19:03:21 -0500

    Create EjIntegrador1_1.java

---
+++ Integrador/EjIntegrador1_1.java
@@ -0,0 +1,20 @@
+class Persona {
+    String nombre;
+    int edad;
+
+    Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    void saludar() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 25);
+        persona1.saludar();
+    }
+}
