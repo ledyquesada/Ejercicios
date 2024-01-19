c88b520b78cd122884b25e4df86b128036b2849c
ledyquesada <ledyquesada@gmail.com>
2024-01-18 21:34:28 -0500

    Create EjInnovador_10.py

---
+++ Innovador/EjInnovador_10.py
@@ -0,0 +1,28 @@
+
+# Busqueda en amplitud (BFS)
+
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            queue.extend(neighbor for neighbor in graph[node] if neighbor not in visited)
+
+# Uso
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F', 'G'],
+    'D': ['B'],
+    'E': ['B'],
+    'F': ['C'],
+    'G': ['C']
+}
+
+bfs(graph, 'A')
