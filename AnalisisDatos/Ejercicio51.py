dc0226e1cc62d39848b24224300dd6b2ae89989f
ledyquesada <ledyquesada@gmail.com>
2024-02-17 18:53:12 -0500

    Create Ejercicio51.py

---
+++ AnalisisDatos/Ejercicio51.py
@@ -0,0 +1,28 @@
+#Bosques aleatorios
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Cargar conjunto de datos Iris
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+# Crear y entrenar el modelo de Bosques Aleatorios
+rf_model = RandomForestClassifier(random_state=42)
+rf_model.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = rf_model.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print(f'Accuracy: {accuracy}')
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+print(f'F1-Score: {f1}')
